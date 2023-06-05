2ae78a59f7073636c1e57d3a66d6a1c59abd21f6
unknown <andreevamailbox@gmail.com>
2023-06-05 12:51:50 +1000

    javascript challenges types operators variables added

---
+++ js_challenges/operators2.js
@@ -0,0 +1,17 @@
+let numberOfDays;
+numberOfDays = 31;
+
+const daysMoonRotate = 29.5306;
+
+let rotations;
+rotations = numberOfDays/daysMoonRotate
+
+console.log(rotations)
+
+const moonOrbitsEarth = 27.3;
+
+console.log(numberOfDays/moonOrbitsEarth)
+
+
+
+
